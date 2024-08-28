401300fc7675ada7a3e729d3fc35fb6332e5dc80
Suraj Kumbhar <110983183+SurajKumbhar905@users.noreply.github.com>
2024-08-28 15:14:59 +0530

    Add IOS Files

---
+++ ios/ReceiveSharingIntent.m
@@ -0,0 +1,19 @@
+#import <React/RCTBridgeModule.h>
+#import <Photos/Photos.h>
+
+
+@interface RCT_EXTERN_MODULE(ReceiveSharingIntent, NSObject)
+
+RCT_EXTERN_METHOD(getFileNames:(NSString)url
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter: (RCTPromiseRejectBlock)reject);
+
+RCT_EXTERN_METHOD(getCheckboxStates:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
+
+RCT_EXTERN_METHOD(clearFileNames)
+RCT_EXTERN_METHOD(clearCheckboxStates)
+
+
+@end
